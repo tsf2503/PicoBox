1a7415677229b71624aacead5e951d13046667f6
TiagoSF <tiagosferreira04@gmail.com>
2025-04-06 20:27:16 +0100

    Refactor button matrix initialization and update modes configuration for improved functionality

--- matrix.py
+++ matrix.py
@@ -22,78 +22,22 @@ class ButtonMatrix:
         - Configures pins as input or output.
         - Initializes internal state for button and switch tracking.
     """
-    def __init__(self, col_pins, row_pins, switch_col_pins, switch_row_pins, MODE):
+    def __init__(self, col_pins, row_pins, MODE):
         # Initialize the buttons
-        self.cols = [digitalio.DigitalInOut(pin) for pin in col_pins]
         self.rows = [digitalio.DigitalInOut(pin) for pin in row_pins]
+        self.cols = [digitalio.DigitalInOut(pin) for pin in col_pins]
 
-        # Initialize the rows for switches
-        self.switch_rows = [digitalio.DigitalInOut(pin) for pin in switch_row_pins]
-
-        # List to hold switch columns (filled in column set loop)
-        self.switch_cols = []
         # List holds the status of each switch
-        self.switch_status = [0] * len(switch_col_pins) * len(switch_row_pins)
+        self.switch_status = [0] * (len(col_pins) * len(row_pins))
 
         # Set columns as output
-        for i, col in enumerate(self.cols):
+        for col in self.cols:
             col.direction = digitalio.Direction.OUTPUT
-            # Checks if the current column has switches; if so, copies the column to the switch list
-            if i in switch_col_pins:
-                self.switch_cols.append(col)
 
         # Set rows as input with pull-down resistors
         for row in self.rows:
             row.direction = digitalio.Direction.INPUT
             row.pull = digitalio.Pull.DOWN
-        for row in self.switch_rows:
-            row.direction = digitalio.Direction.INPUT
-            row.pull = digitalio.Pull.DOWN
-
-        # Sets the mode switcher button
-        self.MODE = MODE
-
-    """
-    Checks the button matrix for pressed buttons.
-
-    Returns:
-        int: The button number that was pressed.
-                -1 for a short press of the mode switcher.
-                -2 for a long press of the mode switcher.
-
-    Side Effects:
-        - Prints debug information about button presses.
-    """
-    def check(self):
-        for col_num, col in enumerate(self.cols):
-            col.value = 1  # Activate the column
-            for row_num, row in enumerate(self.rows):
-                if row.value != 1: 
-                    continue
-
-                # Calculate the button number
-                if col_num == 5:
-                    button_num = 38 + row_num
-                else:
-                    button_num = (row_num + 2) * 5 + col_num + 1
-
-                # Check if the pressed button is the mode switcher
-                if button_num == self.MODE:
-                    start = time.monotonic()
-                    while row.value == 1:
-                        continue
-                    end = time.monotonic()
-                    col.value = 0
-                    # Returns -1 for a short press and -2 for a long press
-                    if end - start < 0.5: 
-                        return -1
-                    else: 
-                        return -2
-
-                time.sleep(0.2)  # Debounce delay
-                col.value = 0
-                return button_num
-            col.value = 0
 
     """
     Checks the switch matrix for changes in switch states.
@@ -105,14 +49,19 @@ class ButtonMatrix:
         - Updates the `switch_status` list.
         - Prints debug information about switch state changes.
     """
-    def SwitchCheck(self):
-        for col_num, col in enumerate(self.switch_cols):
+    def check(self):
+        for col_num, col in enumerate(self.cols):
+            # print("\t Col:", col_num)
             col.value = 1  # Activate the column
-            for row_num, row in enumerate(self.switch_rows):
-                num = row_num * 6 + col_num
-                if row.value == self.switch_status[num]: 
+            for row_num, row in enumerate(self.rows):
+                index = row_num * 6 + col_num
+                value = row.value
+                # print(col_num, row_num, index, self.switch_status[index], row.value)
+                # input("enter:")
+                if value == self.switch_status[index]:
                     continue
-                self.switch_status[num] = row.value
+                self.switch_status[index] = value
+                print("after", col_num, row_num, index, self.switch_status[index], row.value)
 
                 # Calculate the button number
                 if col_num == 5:
@@ -121,7 +70,8 @@ class ButtonMatrix:
                     button_num = row_num * 5 + col_num + 1
 
                 # on press return button number
-                if row.value == 1:
+                col.value = 0
+                if value == 1:
                     return button_num
                 # On release return -button number
                 else:
@@ -174,8 +124,9 @@ class Encoders:
             if self.last[i] != a_current & a_current == 1:
                 # Anticlockwise
                 if a_current != b_current:
-                    self.ClickButton(2 * i + 26)
+                    return(2 * i + 26)
                 # Clockwise
                 else:
-                    self.ClickButton(2 * i + 27)
+                    return(2 * i + 27)
             self.last[i] = a_current
+        return 
